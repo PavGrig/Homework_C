5af3ec87a46c8df754c99c6f0945e32be743183f
Pavel Grigorev <pavel.grigorev.1990@mail.ru>
2022-12-19 00:06:38 +0300

    added a solution to the task about friends and a dog

---
+++ TaskAboutDog/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Distance between friends: ");
+int distance = Convert.ToInt32(Console.ReadLine());
+Console.Write("The dog runs towards a friend: ");
+int friend = Convert.ToInt32(Console.ReadLine());
+Console.Write("speed friend №1: ");
+int friendsspeed1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("speed friend №2: ");
+int friendsspeed2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("speed dog: ");
+int dogspeed = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+int time = 0;
+
+while(distance > 5)
+{
+    if (friend == 1) 
+    {
+        time = distance / (friendsspeed1 + dogspeed);
+        friend = 2;
+    } 
+    else 
+    {
+        time = distance / (friendsspeed2 + dogspeed);
+        friend = 1;
+    }
+    distance = distance - ((friendsspeed1 + friendsspeed2) * time);
+    count++;
+}
+Console.WriteLine(count);
