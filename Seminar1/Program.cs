146f98d51453621451b3077cf04b2631ee6b9ae1
Pavel Grigorev <pavel.grigorev.1990@mail.ru>
2022-12-18 14:28:05 +0300

    added a solution to the second task

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -16,4 +16,17 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 if (num1 > num2) Console.WriteLine ("The first number is maximum, the second number is minimum");
 else Console.WriteLine ("The second number is maximum, the first number is minimum");*/
 
+//Задача 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
+/*Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.WriteLine("maximum: " + max);*/
