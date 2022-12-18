ae1ed89911945fafba94d5b772034664f24c8ac3
Pavel Grigorev <pavel.grigorev.1990@mail.ru>
2022-12-18 14:40:02 +0300

    added a solution to the third task

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -7,18 +7,20 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 
 if (num1 > num2) Console.WriteLine ("The first number is greater than the second number");
 else Console.WriteLine ("The second number is greater than the first number");*/
-
-/*Console.Write("Input a first number: ");
+/*
+Console.Write("Input a first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2) Console.WriteLine ("The first number is maximum, the second number is minimum");
-else Console.WriteLine ("The second number is maximum, the first number is minimum");*/
+if (num1 > num2) Console.WriteLine ("The first number is maximum: " + num1 + ", the second number is minimum: " + num2);
+else Console.WriteLine ("The second number is maximum: " + num2 + ", the first number is minimum: " + num1);
+*/
 
 //Задача 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-/*Console.Write("Input a first number: ");
+/*
+Console.Write("Input a first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
@@ -29,4 +31,15 @@ int max = num1;
 if (num2 > max) max = num2;
 if (num3 > max) max = num3;
 
-Console.WriteLine("maximum: " + max);*/
+Console.WriteLine("maximum: " + max);
+*/
+
+//Задача 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+/*
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num %2 == 0) Console.WriteLine("The number is even.");
+else Console.WriteLine("The number is odd");
+*/
